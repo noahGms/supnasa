5edf952ce7b940539d7caad86c99caa96a0eb4c8
noahGms <noah@keshin.net>
2022-03-29 20:56:46 +0200

    added update and delete route for users

--- src/modules/users/controller.js
+++ src/modules/users/controller.js
@@ -1,6 +1,6 @@
 const User = require("./model");
 const {returnData, returnError, returnSuccess} = require("../../helpers/responses");
-const {userCreateSchema} = require("./validation");
+const {userCreateSchema, userUpdateSchema} = require("./validation");
 
 exports.index = async (req, res) => {
   const users = await User.find();
@@ -26,4 +26,41 @@ exports.store = async (req, res) => {
   }
 
   return returnSuccess(res, "User created successfully");
+};
+
+exports.update = async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return returnError(res, "User not found");
+  }
+
+  if (!user._id.equals(req.user._id) && !req.user.isAdmin) {
+    return returnError(res, "You can't update this user");
+  }
+
+  try {
+    const result = await userUpdateSchema.validateAsync(req.body);
+    await User.findByIdAndUpdate(req.params.id, result);
+  } catch (error) {
+    return returnError(res, error.message);
+  }
+
+  return returnData(res, "User updated successfully");
+};
+
+exports.destroy = async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return returnError(res, "User not found");
+  }
+
+  if (!user._id.equals(req.user._id)) {
+    return returnError(res, "You can't delete this user");
+  }
+
+  await User.findByIdAndDelete(req.params.id);
+
+  return returnSuccess(res, "User deleted successfully");
 };
--- src/modules/users/route.js
+++ src/modules/users/route.js
@@ -1,10 +1,13 @@
 const {Router} = require('express');
-const {index, store, show} = require("./controller");
+const {index, store, show, update, destroy} = require("./controller");
+const {isAuth} = require("../../middlewares/auth");
 
 const router = Router();
 
-router.get('/', index);
-router.get('/:id', show);
+router.get('/', isAuth, index);
+router.get('/:id', isAuth, show);
 router.post('/', store);
+router.put('/:id', isAuth, update);
+router.delete('/:id', isAuth, destroy);
 
 module.exports = router;
