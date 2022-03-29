c722f9a2ec266f53098d598c6866aa393d61dd2d
noahGms <noah@keshin.net>
2022-03-29 13:44:19 +0200

    setup minimal express server with helmet and morgan middleware

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("hello world");
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+function main() {
+  const server = express();
+
+  server.use(express.json());
+  server.use(express.urlencoded({ extended: true }));
+  server.use(helmet());
+  server.use(morgan("common"));
+
+  server.get("/", (req, res) => {
+    res.json({ message: "Entry point of SUPNASA api !" });
+  });
+
+  const port = process.env.PORT || 3000;
+  server.listen(port, () => {
+    console.log(`Express server listening on port ${port} !`);
+  });
+}
+main();
