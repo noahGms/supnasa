c4a76458ae1e1dd9d1a9ab5e94c76ab54377bb8e
noahGms <noah@keshin.net>
2022-03-30 13:37:14 +0200

    added filters to index route of rovers

--- src/modules/rovers/controller.js
+++ src/modules/rovers/controller.js
@@ -3,7 +3,23 @@ const Rover = require("./model");
 const {roverCreateSchema, roverUpdateSchema} = require("./validation");
 
 exports.index = async function (req, res) {
-  const rovers = await Rover.find();
+  let limit = 10;
+  let sort = {};
+
+  if (req.query.limit) {
+    limit = parseInt(req.query.limit);
+  }
+
+  if (req.query.name) {
+    sort.name = req.query.name;
+  }
+
+  if (req.query.date) {
+    sort.launchDate = req.query.date;
+    sort.constructionDate = req.query.date;
+  }
+
+  const rovers = await Rover.find().sort({...sort}).limit(limit);
   return returnData(res, rovers);
 };
 
